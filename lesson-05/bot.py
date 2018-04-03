e380d511f94a55f20a915eeb99aa4cddf2b13431
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-04-03 14:01:00 +0300

    Lesson 05 complete

---
+++ lesson-05/bot.py
@@ -0,0 +1,136 @@
+from aiogram import Bot, types
+from aiogram.utils import executor
+from aiogram.utils.markdown import text
+from aiogram.dispatcher import Dispatcher
+
+from config import TOKEN
+import keyboards as kb
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+##
+
+
+@dp.callback_query_handler(func=lambda c: c.data == 'button1')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+
+
+@dp.callback_query_handler(func=lambda c: c.data and c.data.startswith('btn'))
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    code = callback_query.data[-1]
+    if code.isdigit():
+        code = int(code)
+    if code == 2:
+        await bot.answer_callback_query(callback_query.id, text='Нажата вторая кнопка')
+    elif code == 5:
+        await bot.answer_callback_query(
+            callback_query.id,
+            text='Нажата кнопка с номером 5.\nА этот текст может быть длиной до 200 символов 😉',
+            show_alert=True)
+    else:
+        await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, f'Нажата инлайн кнопка! code={code}')
+
+
+##
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!", reply_markup=kb.greet_kb)
+
+
+@dp.message_handler(commands=['hi1'])
+async def process_hi1_command(message: types.Message):
+    await message.reply("Первое - изменяем размер клавиатуры",
+                        reply_markup=kb.greet_kb1)
+
+
+@dp.message_handler(commands=['hi2'])
+async def process_hi2_command(message: types.Message):
+    await message.reply("Второе - прячем клавиатуру после одного нажатия",
+                        reply_markup=kb.greet_kb2)
+
+
+@dp.message_handler(commands=['hi3'])
+async def process_hi3_command(message: types.Message):
+    await message.reply("Третье - добавляем больше кнопок",
+                        reply_markup=kb.markup3)
+
+
+@dp.message_handler(commands=['hi4'])
+async def process_hi4_command(message: types.Message):
+    await message.reply("Четвертое - расставляем кнопки в ряд",
+                        reply_markup=kb.markup4)
+
+
+@dp.message_handler(commands=['hi5'])
+async def process_hi5_command(message: types.Message):
+    await message.reply("Пятое - добавляем ряды кнопок",
+                        reply_markup=kb.markup5)
+
+
+@dp.message_handler(commands=['hi6'])
+async def process_hi6_command(message: types.Message):
+    await message.reply("Шестое - запрашиваем контакт и геолокацию\n"
+                        "Эти две кнопки не зависят друг от друга",
+                        reply_markup=kb.markup_request)
+
+
+@dp.message_handler(commands=['hi7'])
+async def process_hi7_command(message: types.Message):
+    await message.reply("Восьмое - все методы вместе",
+                        reply_markup=kb.markup_big)
+
+
+@dp.message_handler(commands=['rm'])
+async def process_rm_command(message: types.Message):
+    await message.reply("Убираем шаблоны сообщений",
+                        reply_markup=kb.ReplyKeyboardRemove())
+
+##
+
+
+@dp.message_handler(commands=['1'])
+async def process_command_1(message: types.Message):
+    await message.reply("Первая инлайн кнопка",
+                        reply_markup=kb.inline_kb1)
+
+
+@dp.message_handler(commands=['2'])
+async def process_command_2(message: types.Message):
+    await message.reply("Отправляю все возможные кнопки",
+                        reply_markup=kb.inline_kb_full)
+
+help_message = text(
+    "Это урок по клавиатурам.",
+    "Доступные команды:\n",
+    "/start - приветствие",
+    "\nШаблоны клавиатур:",
+    "/hi1 - авто размер",
+    "/hi2 - скрыть после нажатия",
+    "/hi3 - больше кнопок",
+    "/hi4 - кнопки в ряд",
+    "/hi5 - больше рядов",
+    "/hi6 - запрос локации и номера телефона",
+    "/hi7 - все методы"
+    "/rm - убрать шаблоны",
+    "\nИнлайн клавиатуры:",
+    "/1 - первая кнопка",
+    "/2 - сразу много кнопок",
+    sep="\n"
+)
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply(help_message)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ lesson-05/keyboards.py
@@ -0,0 +1,73 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+
+button_hi = KeyboardButton('Привет! 👋')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+
+greet_kb1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
+
+greet_kb2 = ReplyKeyboardMarkup(
+    resize_keyboard=True, one_time_keyboard=True
+).add(button_hi)
+
+button1 = KeyboardButton('1️⃣')
+button2 = KeyboardButton('2️⃣')
+button3 = KeyboardButton('3️⃣')
+
+markup3 = ReplyKeyboardMarkup().add(
+    button1).add(button2).add(button3)
+
+markup4 = ReplyKeyboardMarkup().row(
+    button1, button2, button3
+)
+
+markup5 = ReplyKeyboardMarkup().row(
+    button1, button2, button3
+).add(KeyboardButton('Средний ряд'))
+
+button4 = KeyboardButton('4️⃣')
+button5 = KeyboardButton('5️⃣')
+button6 = KeyboardButton('6️⃣')
+markup5.row(button4, button5)
+markup5.insert(button6)
+
+markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('Отправить свой контакт ☎️', request_contact=True)
+).add(
+    KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
+)
+
+markup_big = ReplyKeyboardMarkup()
+
+markup_big.add(
+    button1, button2, button3, button4, button5, button6
+)
+markup_big.row(
+    button1, button2, button3, button4, button5, button6
+)
+
+markup_big.row(button4, button2)
+markup_big.add(button3, button2)
+markup_big.insert(button1)
+markup_big.insert(button6)
+markup_big.insert(KeyboardButton('9️⃣'))
+
+
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+
+inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
+inline_kb_full.add(InlineKeyboardButton('Вторая кнопка', callback_data='btn2'))
+inline_btn_3 = InlineKeyboardButton('кнопка 3', callback_data='btn3')
+inline_btn_4 = InlineKeyboardButton('кнопка 4', callback_data='btn4')
+inline_btn_5 = InlineKeyboardButton('кнопка 5', callback_data='btn5')
+inline_kb_full.add(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.insert(InlineKeyboardButton("query=''", switch_inline_query=''))
+inline_kb_full.insert(InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
+inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате", switch_inline_query_current_chat='wasd'))
+inline_kb_full.add(InlineKeyboardButton('Уроки aiogram', url='https://surik00.gitbooks.io/aiogram-lessons/content/'))
